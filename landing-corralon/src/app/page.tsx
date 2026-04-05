c7afa6af0f9a76ef12991a964a44f01ca3673f7c
Ezequiel Grigolatto <ezequiel@MacBook-Pro-de-Ezequiel.local>
2026-04-05 00:57:56 -0300

    componente scrollable y tag

--- landing-corralon/src/app/page.tsx
+++ landing-corralon/src/app/page.tsx
@@ -1,5 +1,7 @@
 import { Button } from '@ui';
 import Hero from '@ui/Hero';
+import ScrollableSection from '@ui/ScrollableSection';
+import Tag from '@ui/Tag';
 import TestimonialsSection from '@ui/TestimonialsSection';
 
 export default function Home() {
@@ -28,7 +30,36 @@ export default function Home() {
         backgroundImage="https://as2.ftcdn.net/v2/jpg/03/39/67/57/1000_F_339675724_zKIsiEcSss6x2KOXUfHMfBrK9b0qbYCQ.jpg"
         mobileBackgroundImage="https://as2.ftcdn.net/v2/jpg/03/39/67/57/1000_F_339675724_zKIsiEcSss6x2KOXUfHMfBrK9b0qbYCQ.jpg"
       />
-      <TestimonialsSection title="What our customers say" cards={[]} />
+      <div className="max-w-7xl">
+        <TestimonialsSection title="What our customers say" cards={[]} />
+        <ScrollableSection
+          tag={<Tag text="EL PROBLEMA" variant="outline" />}
+          title="Con F5 te ayudamos a que recuperes el control de tu negocio en un solo lugar"
+          subsections={[
+            {
+              title: 'Gestión manual y desorganizada',
+              description:
+                'Muchos corralones y ferreterías aún dependen de métodos tradicionales como papel y lápiz para llevar el control de inventarios, ventas y finanzas, lo que resulta en errores, pérdida de información y falta de eficiencia.',
+              image:
+                'https://ferreteriamlo.com.ar/wp-content/uploads/2022/01/louis-hansel-Rf9eElW3Qxo-unsplash.jpg',
+            },
+            {
+              title: 'Dificultad para competir',
+              description:
+                'La falta de digitalización limita la capacidad de los corralones y ferreterías para competir con empresas más grandes y tecnológicamente avanzadas, lo que puede resultar en pérdida de clientes y oportunidades de crecimiento.',
+              image:
+                'https://ferreteriamlo.com.ar/wp-content/uploads/2022/01/pexels-pixabay-162534-600x400.jpg',
+            },
+            {
+              title: 'Falta de visibilidad y control',
+              description:
+                'Sin un sistema digital, los propietarios y gerentes de corralones y ferreterías carecen de visibilidad en tiempo real sobre sus operaciones, lo que dificulta la toma de decisiones informadas y la identificación de áreas de mejora.',
+              image:
+                'https://ferreteriamlo.com.ar/wp-content/uploads/2022/01/pexels-lisa-1301856-600x400.jpg',
+            },
+          ]}
+        />
+      </div>
     </div>
   );
 }
--- ui/src/index.ts
+++ ui/src/index.ts
@@ -1,3 +1,5 @@
 export * from './components/ui/button';
 export * from './components/Hero';
 export * from './components/TestimonialsSection';
+export * from './components/Tag';
+export * from './components/ScrollableSection';
