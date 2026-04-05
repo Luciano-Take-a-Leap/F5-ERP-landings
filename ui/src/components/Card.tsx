c40dd0fe3f33683dd37a3eda420158bcd75953c9
Ezequiel Grigolatto <ezequiel@MacBook-Pro-de-Ezequiel.local>
2026-04-05 15:47:15 -0300

    ajustes ui

--- ui/src/components/Card.tsx
+++ ui/src/components/Card.tsx
@@ -6,7 +6,7 @@ export interface CardProps {
 
 const Card: React.FC<CardProps> = ({ icon, title, description }) => {
   return (
-    <div className="rounded-2xl border border-card-from bg-gradient-to-br from-card-from via-card-from to-card-to p-8">
+    <div className="rounded-2xl border border-primary/20 p-8 card-gradient">
       <div className="mb-6 flex h-12 w-12 items-center justify-center rounded-xl bg-icon-bg text-primary font-extrabold">
         {icon}
       </div>
